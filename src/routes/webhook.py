7e0b674baaf12dcbaba28c734eacc64f8f28200f
justdoit4082 <126318767+justdoit4082@users.noreply.github.com>
2025-06-29 19:13:17 +0800

    Update webhook.py

--- src/routes/webhook.py
+++ src/routes/webhook.py
@@ -8,9 +8,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, JoinEvent, FollowEvent,
     MemberJoinedEvent, MemberLeftEvent, UnfollowEvent, LeaveEvent
 )
-# from src.utils.check_admin import is_user_group_admin
-# from src.utils.create_log import create_event_log
-# from src.utils.reply_message import reply_text_message
+from src.utils.check_admin import is_user_group_admin
+from src.utils.create_log import create_event_log
+from src.utils.reply_message import reply_text_message
 
 webhook_bp = Blueprint('webhook', __name__)
 
