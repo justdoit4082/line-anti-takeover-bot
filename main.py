39a4c96be85a4d0401ed21f01e0e1aaea2907ee5
justdoit4082 <126318767+justdoit4082@users.noreply.github.com>
2025-06-29 22:29:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 app.register_blueprint(webhook_bp, url_prefix="/callback")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
