7ec64b8e067f0bfc5536f5c7e07220415dbf3ae3
justdoit4082 <126318767+justdoit4082@users.noreply.github.com>
2025-06-30 08:57:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from src.webhook import webhook_bp
+from src.routes.webhook import webhook_bp
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False  # <<<<< 加這一行！
+app.register_blueprint(webhook_bp)
 
-app.register_blueprint(webhook_bp, url_prefix="/callback")
+@app.route("/")
+def home():
+    return "LINE Anti-Takeover Bot is running."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
